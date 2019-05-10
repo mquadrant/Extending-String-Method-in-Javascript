b30f4284e2145c8031073f169d2800f3e6461652
Benjamin Mark <mquadrant@gmail.com>
2019-05-10 13:53:15 +0100

    wrote a test case to test the ucFirst  method in the string prototype

--- test/methods.test.js
+++ test/methods.test.js
@@ -1,20 +1,27 @@
-describe("Test for hasVowel method in the String Prototype", () => {
+describe("Test for hasVowel() method in the String Prototype", () => {
     it("should returns 'true' if the string contains vowels", () => {
         var result = "Going home".hasVowel();
         expect(result).toBeTruthy();
     });
 });
 
-describe("Test for toUpper method in String Prototype Returns the String", () => {
+describe("Test for toUpper() method in String Prototype", () => {
     it("should returns the passed-in string with all characters in upper cases", () => {
         var result = "Going 3467 home".toUpper();
         expect(result).toMatch("GOING 3467 HOME");
     });
 });
 
-describe("Test for toUpper method in String Prototype Returns the String", () => {
-    it("should returns the passed-in string with all characters in upper cases", () => {
-        var result = "Going 3467 home".toUpper();
-        expect(result).toMatch("GOING 3467 HOME");
+describe("Test for toLower() method in String Prototype", () => {
+    it("should returns the passed-in string with all characters in lower cases", () => {
+        var result = "GoinG 3467 hoMe".toLower();
+        expect(result).toMatch("going 3467 home");
+    });
+});
+
+describe("Test for ucFirst() method in String Prototype", () => {
+    it("should returns the passed-in string with all the First Character change to Upper case", () => {
+        var result = "house is good".ucFirst();
+        expect(result).toMatch("House is good");
     });
 });
