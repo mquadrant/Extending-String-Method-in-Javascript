a380b8389a143c323319532db6635f8c46a7ae21
Benjamin Mark <mquadrant@gmail.com>
2019-05-10 14:15:57 +0100

    Wrote a test case on the wordCount() method on string prototype

--- test/methods.test.js
+++ test/methods.test.js
@@ -8,21 +8,21 @@ describe("Test for hasVowel() method in the String Prototype", () => {
 describe("Test for toUpper() method in String Prototype", () => {
     it("should return the passed-in string with all characters in upper cases", () => {
         var result = "Going 3467 home".toUpper();
-        expect(result).toMatch("GOING 3467 HOME");
+        expect(result).toEqual("GOING 3467 HOME");
     });
 });
 
 describe("Test for toLower() method in String Prototype", () => {
     it("should return the passed-in string with all characters in lower cases", () => {
         var result = "GoinG 3467 hoMe".toLower();
-        expect(result).toMatch("going 3467 home");
+        expect(result).toEqual("going 3467 home");
     });
 });
 
 describe("Test for ucFirst() method in String Prototype", () => {
     it("should return the passed-in string with all the First Character change to Upper case", () => {
         var result = "house is good".ucFirst();
-        expect(result).toMatch("House is good");
+        expect(result).toEqual("House is good");
     });
 });
 
@@ -36,6 +36,13 @@ describe("Test for isQuestion() method in String Prototype", () => {
 describe("Test for words() method in String Prototype", () => {
     it("should return a list of the words in the string, as an Array.", () => {
         var result = "Javascript is a programming language.".words();
-        expect(result).toMatch(['Javascript', 'is', 'a', 'programming', 'language']);
+        expect(result).toEqual(['Javascript', 'is', 'a', 'programming', 'language']);
+    });
+});
+
+describe("Test for wordCount() method in String Prototype", () => {
+    it("should return the number of words in the string", () => {
+        var result = "Javascript is a programming language.".wordCount();
+        expect(result).toBe(5);
     });
 });
