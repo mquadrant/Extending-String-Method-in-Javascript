ae8c91b9fc964208c772652727ea87a34411dc0b
Benjamin Mark <mquadrant@gmail.com>
2019-05-10 18:24:55 +0100

    Created toUpper method on string prototype to change lowercase to uppercase

--- method/methods.js
+++ method/methods.js
@@ -1,6 +1,13 @@
+//hasvowel return true if the string contain a vowel
 String.prototype.hasVowel = function(){
     return /[aeiou]/.test(this);
 }
 
+//toUpper return the string in capital case
+String.prototype.toUpper = function(){  
+    return this.replace(/[a-z]/g, function(str){
+        return String.fromCharCode(str.charCodeAt(0) - 32);
+    });
+}
 
 module.exports = String.prototype;
