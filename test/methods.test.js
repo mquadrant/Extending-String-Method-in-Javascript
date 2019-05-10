4a0f44bab0b9a4bc074db11f0d02a8080841dd00
Benjamin Mark <mquadrant@gmail.com>
2019-05-10 12:45:58 +0100

    wrote a test case to test the toUpper method in the string  prototype

--- test/methods.test.js
+++ test/methods.test.js
@@ -4,3 +4,10 @@ describe("Test for hasVowel method in the String Prototype", () => {
         expect(result).toBeTruthy();
     });
 });
+
+describe("Test for toUpper method in String Prototype Returns the String", () => {
+    it("should returns the passed-in string with all characters in upper cases", () => {
+        var result = "Going 3467 home".toUpper();
+        expect(result).toMatch("GOING 3467 HOME");
+    });
+});
